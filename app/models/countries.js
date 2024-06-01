b638e44fe07c21fa956e22c5c870fa35e273ae07
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2024-06-01 21:18:52 +0530

    fix url

--- app/models/countries.js
+++ app/models/countries.js
@@ -1,14 +1,14 @@
 const getCountriesForShop = async session => {
   console.log(process.env.SERVER_URL)
   console.log(process.env)
-  const res = await fetch("https://ipblocker.shopifyplugins.pro" + "ipblocker/countries.json?token=" + session)
+  const res = await fetch("https://ipblocker.shopifyplugins.pro/" + "ipblocker/countries.json?token=" + session)
   const countries = await res.json()
   return countries["countries"]
 }
 const addCountryToShop = async (token, country, countryCode) => {
   console.log(process.env.SERVER_URL)
   console.log(process.env)
-  fetch("https://ipblocker.shopifyplugins.pro" + "ipblocker/countries.json", {
+  fetch("https://ipblocker.shopifyplugins.pro/" + "ipblocker/countries.json", {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
