b228c58282dd5e95a8939a44b8b95b86d84923b9
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2024-06-02 18:28:35 +0530

    debug

--- app/routes/countries.js
+++ app/routes/countries.js
@@ -1,16 +1,16 @@
 import { json } from '@remix-run/node';
 import { getCountriesForShop } from '../models/countries';
-// import prisma from '../db.server';
+import prisma from '../db.server';
 
 export const loader = async ({ params }) => {
-//   const shop = params.shop
-//   const session = await prisma.ipblockerSession.findMany({
-//     where: {
-//       shop: shop
-//     }
-//   })
+  const shop = params.shop
+  const session = await prisma.ipblockerSession.findMany({
+    where: {
+      shop: shop
+    }
+  })
 
-  const countries = await getCountriesForShop()//session[0].accessToken)
+  const countries = await getCountriesForShop(session[0].accessToken)
    return json(
      { countries }    
    );
