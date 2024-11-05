c95ca8b569815d8fee3a5f608f56600689bb3f2b
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2024-11-05 20:24:40 +0530

    fix domain

--- extensions/fraud-blocker/src/Checkout.jsx
+++ extensions/fraud-blocker/src/Checkout.jsx
@@ -40,7 +40,7 @@ function Extension() {
     const checkAllowed = async () => {
       try {
         const response = await fetch(
-          "https://ipblocker.valuecommerce.com/checkout",
+          "https://ipblocker.valuecommerce.info/checkout",
           {
             method: "POST",
             headers: {
