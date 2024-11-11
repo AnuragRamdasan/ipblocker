ae6118c7480c5d59474cca4c71f6493636b932ad
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2024-11-11 19:47:10 +0530

    add code to loader too

--- app/routes/checkout.js
+++ app/routes/checkout.js
@@ -3,7 +3,34 @@ import { getCheckoutStatusForShop } from "../models/countries";
 import prisma from "../db.server";
 
 export const loader = async ({ request }) => {
-  return json({});
+  // Get the request body
+  const body = await request.json();
+  if (!body.checkout_config || !body.shop) {
+    return json({});
+  }
+
+  const session = await prisma.ipblockerSession.findMany({
+    where: {
+      shop: body.shop,
+    },
+  });
+
+  const allowed = await getCheckoutStatusForShop(
+    session[0].accessToken,
+    body.checkout_config,
+  );
+
+  return json(
+    { allowed: allowed.allowed, reason: allowed.reason },
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Content-Type": "application/json",
+      },
+    },
+  );
 };
 
 export const action = async ({ request }) => {
